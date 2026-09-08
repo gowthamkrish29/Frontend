cc18242525af090739b41b615e84f063766d8445
gowthamkrish29 <krishgowtham29@gmail.com>
2026-09-08 21:33:22 +0530

    Updated

---
+++ JAVASCRIPT/Day16/assets/js/task3.js
@@ -0,0 +1,9 @@
+const heading = document.getElementById("header");
+
+const button = document.getElementById("submit");
+
+button.addEventListener("click", ()=>{
+
+    header.classList.toggle('show')
+
+});
---
+++ JAVASCRIPT/Day16/assets/js/task4.js
@@ -0,0 +1,9 @@
+const text = document.getElementById("input");
+
+const button = document.getElementById("submit");
+
+button.addEventListener("click", ()=>{
+
+    console.log(input.value);
+
+});
